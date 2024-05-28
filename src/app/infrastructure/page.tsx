21779ad5a4f66bc984b84c2d742224eaabc0d34a
Mostapha <darsh.taha12382@gmail.com>
2024-05-28 07:05:33 +0300

    Update Project.1.5

--- src/app/infrastructure/page.tsx
+++ src/app/infrastructure/page.tsx
@@ -8,7 +8,7 @@ const Infrastructure = () => {
 
                 <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 grid-cols-2 max-[577px]:grid-cols-1 gap-5">
                     <div className="w-[250px] h-[250px] bg-white rounded-xl shadow-lg grid justify-center items-center">
-                        <Link href="/driver" className="grid items-center justify-center text-center" >
+                        <Link href="/bus" className="grid items-center justify-center text-center" >
                             <div className="bg-[#FAEFEF] rounded-full h-[87px] w-[87px] grid items-center justify-center ">
                                 <svg className="h-12 w-12 text-[#000000] group-hover:text-[#3e5af0]" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <circle cx="6" cy="17" r="2" />  <circle cx="18" cy="17" r="2" />  <path d="M4 17h-2v-11a1 1 0 0 1 1 -1h14a5 7 0 0 1 5 7v5h-2m-4 0h-8" />  <polyline points="16 5 17.5 12 22 12" />  <line x1="2" y1="10" x2="17" y2="10" />  <line x1="7" y1="5" x2="7" y2="10" />  <line x1="12" y1="5" x2="12" y2="10" /></svg>
                             </div>
@@ -24,7 +24,7 @@ const Infrastructure = () => {
                         </Link>
                     </div>
                     <div className="w-[250px] h-[250px] bg-white rounded-xl shadow-lg grid justify-center items-center">
-                        <Link href="/parent" className="grid items-center justify-center text-center" >
+                        <Link href="/book" className="grid items-center justify-center text-center" >
                             <div className="bg-[#FAEFEF] rounded-full h-[87px] w-[87px] grid items-center justify-center ">
                             <svg className="h-11 w-11 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"/></svg>
                             </div>
