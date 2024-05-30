2a9c087e5e82994f95d00bea3531cacbb0d4e596
Mostapha <darsh.taha12382@gmail.com>
2024-05-30 15:25:57 +0300

    Dashboard

--- src/app/Dashboard/dashboard.tsx
+++ src/app/Dashboard/dashboard.tsx
@@ -3,21 +3,21 @@
 import dynamic from 'next/dynamic';
 import Calendar from '@/components/calendar';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-// Dynamically import ReactApexChart with SSR disabled
 const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 interface ApexChartProps {}
 
 const Dashboard: React.FC<ApexChartProps> = () => {
+
   const [series, setSeries] = useState([
     {
-      name: 'series1',
+      name: 'Expense',
       data: [31, 40, 28, 51, 42, 109, 100]
     },
     {
-      name: 'series2',
+      name: 'Income',
       data: [11, 32, 45, 32, 34, 52, 41]
     }
   ]);
@@ -25,6 +25,7 @@ const Dashboard: React.FC<ApexChartProps> = () => {
   const [options, setOptions] = useState({
     chart: {
       height: 350,
+      width: 800,
       type: 'area' as 'area'
     },
     colors: ['#FF4560', '#008FFB'],
@@ -55,37 +56,41 @@ const Dashboard: React.FC<ApexChartProps> = () => {
 
 
   return (
-    <div className="grid mr-10 justify-center w-full ">
-
-      <div className="flex justify-evenly mb-6 w-full ">
-        <div className="bg-white rounded-xl p-2 w-[201px] h-[80px] justify-center items-center shadow-xl">
+    <div className="grid mr-10 justify-center w-full overflow-x-auto p-6">
+<div className="grid overflow-x-auto">
+      <div className="flex w-full justify-evenly gap-4 mb-6  max-[812px]:justify-center max-[576px]:h-[120px] whitespace-nowrap">
+        <div className="bg-white rounded-xl p-2 w-[201px] h-[80px] justify-center items-center shadow-xl max-[576px]:h-[100px]">
           <p className="text-gray-400 text-[12px]">Total student</p>
           <h1 className="text-[17px] font-semibold">12.130K</h1>
           <h1 className="text-gray-400 text-[12px]"> <span className="text-green-500 font-semibold">4.63%</span> vs. last Year</h1>
         </div>
-        <div className="bg-white rounded-xl p-2 w-[201px] h-[80px] justify-center items-center shadow-xl">
+        <div className="bg-white rounded-xl p-2 w-[201px] h-[80px] justify-center items-center shadow-xl max-[576px]:h-[100px]">
           <p className="text-gray-400 text-[12px]">Total student</p>
           <h1 className="text-[17px] font-semibold">12.130K</h1>
           <h1 className="text-gray-400 text-[12px]"> <span className="text-green-500 font-semibold">4.63%</span> vs. last Year</h1>
         </div>
-        <div className="bg-white rounded-xl p-2 w-[201px] h-[80px] justify-center items-center shadow-xl">
+        <div className="bg-white rounded-xl p-2 w-[201px] h-[80px] justify-center items-center shadow-xl max-[576px]:h-[100px]">
           <p className="text-gray-400 text-[12px]">Total student</p>
           <h1 className="text-[17px] font-semibold">12.130K</h1>
           <h1 className="text-gray-400 text-[12px]"> <span className="text-green-500 font-semibold">4.63%</span> vs. last Year</h1>
         </div>
-        <div className="bg-white rounded-xl p-2 w-[201px] h-[80px] justify-center items-center shadow-xl">
+        <div className="bg-white rounded-xl p-2 w-[201px] h-[80px] justify-center items-center shadow-xl max-[576px]:h-[100px]">
           <p className="text-gray-400 text-[12px]">Total student</p>
           <h1 className="text-[17px] font-semibold">12.130K</h1>
           <h1 className="text-gray-400 text-[12px]"> <span className="text-green-500 font-semibold">4.63%</span> vs. last Year</h1>
         </div>
       </div>
+</div>
 
-      <div className="flex mb-6 justify-between gap-10 w-full">
-        <div id="chart" className='bg-white rounded-xl p-2 w-[850px] shadow-xl'>
-          <p className="font-semibold text-[18px]">School Finace</p>
-          <ReactApexChart options={options} series={series} type="area" width={800} height={350} />
+      <div className="2xl:flex grid grid-cols-1 mb-6 justify-between gap-10 w-full overflow-x-auto ">
+        <div className="flex overflow-x-auto">
+          <div id="chart" className='bg-white rounded-xl p-2 w-[850px] shadow-xl overflow-x-auto'>
+            <p className="font-semibold text-[18px]">School Finace</p>
+            <ReactApexChart options={options} series={series} type="area" width={options.chart.width} height={options.chart.height} />
+          </div>
         </div>
-        <div className='grid bg-white justify-center items-center p-2 rounded-xl w-[550px] shadow-xl'>
+        <div className="grid overflow-x-auto">
+        <div className='grid bg-white justify-center items-center p-2 rounded-xl w-[550px] shadow-xl max-[1536px]:w-[850px] max-[1536px]:h-[450px] overflow-x-auto'>
           <div className="flex justify-evenly items-center">
             <div className="p-2 bg-[#F9DCA4] h-[75px] w-[66px] rounded-xl justify-center items-center text-center mr-3">
               <h1 className="text-[18px] font-semibold text-[#F79009]">6</h1>
@@ -150,13 +155,19 @@ const Dashboard: React.FC<ApexChartProps> = () => {
               <button className="px-1 py-1.5 whitespace-nowrap rounded-xl bg-[#3E5AF0] hover:bg-[#4a5cc5] hover:shadow-xl mb-5 mr-3 text-white text-[14px] w-[120px] ease-in font-semibold duration-300">+ New Event</button>
           </div>
         </div>
+        </div>
       </div>
 
-      <div className="flex mb-6 justify-between gap-10 w-full">
-        <div className="shadow-xl w-[850px] flex justify-center bg-white">
+      <div className="2xl:flex grid grid-cols-1 mb-6 justify-between gap-10 w-full overflow-x-auto">
+<div className="grid overflow-x-auto">
+
+        <div className="shadow-xl w-[850px] max-[1536px]:w-full flex justify-center bg-white overflow-x-auto">
           <Calendar/>
         </div>
-        <div className="grid bg-white rounded-xl shadow-xl p-2 w-[550px]">
+        </div>
+<div className="grid overflow-x-auto">
+
+        <div className="grid bg-white rounded-xl shadow-xl p-2 w-[550px]  max-[1536px]:w-full overflow-x-auto">
           <p className="font-bold text-[20px]">Notice Board</p>
           <div className="">
           <h1 className="font-semibold text-[18px] text-[#F79009]">Leslie Alexander</h1>
@@ -187,6 +198,7 @@ const Dashboard: React.FC<ApexChartProps> = () => {
           <p>
           In a laoreet purus. Integer turpis quam, laoreet id orci nec, ultrices lacinia nunc. Aliquam erat vo
           </p>
+          </div>
           </div>
         </div>
       </div>
