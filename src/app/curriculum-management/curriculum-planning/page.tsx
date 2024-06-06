43b11bbeae179b62aaa9044dcf030589b329a815
Mostapha <darsh.taha12382@gmail.com>
2024-06-06 15:21:48 +0300

    Three pages

--- src/app/curriculum-management/curriculum-planning/page.tsx
+++ src/app/curriculum-management/curriculum-planning/page.tsx
@@ -1,9 +1,110 @@
 const CurriculumPlanning = () => {
-    return ( 
+    return (
         <>
-            <div></div>
+<div className="lg:ml-[290px] mt-12 ">
+                <div className="flex justify-between max-[502px]:grid max-[502px]:justify-center text-center w-full px-8">
+                    <div className="mb-3">
+                        <label htmlFor="icon" className="sr-only">Search</label>
+                        <div className="relative min-w-72 md:min-w-80">
+                            <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+                                <svg className="flex-shrink-0 size-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>
+                            </div>
+                            <input type="text" id="icon" name="icon" className="py-2  outline-none border-2 px-4 ps-11 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Search" />
+                        </div>
+                    </div>
+                    <div className="flex justify-center">
+                        <button className="px-4 py-2 whitespace-nowrap rounded-xl bg-[#3E5AF0] hover:bg-[#4a5cc5] hover:shadow-xl mb-5 mr-3 text-white text-[18px] w-[180px] ease-in font-semibold duration-300">6-Jun-2024</button>
+                    </div>
+                </div>
+                <div className="flex h-full w-full justify-center items-center p-5 overflow-hidden">
+                    <div className="grid bg-white rounded-xl h-full w-full">
+                        <div className="w-full bg-[#DCEBFB] flex justify-evenly p-2 pl-8 items-center h-[60px] rounded-t-xl">
+                            <div className="w-full text-start">
+                                <p className="font-semibold">Course Title:</p>
+                            </div>
+                            <div className="w-full text-center">
+                                <p className="font-semibold">Course Number:</p>
+                            </div>
+                        </div>
+                        <div className="pl-8">
+                            <p className="font-semibold">Course Description:</p>
+                        </div>
+                        <div className="overflow-auto">
+                        <div className="overflow-auto relative ">
+                    <table className="w-full overflow-x-auto text-sm text-left rtl:text-right text-gray-500 ">
+                        <thead>
+                            <tr className="bg-white  hover:bg-gray-50 text-black font-semibold">
+                                
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap border-2 border-[#dcebfb] ">
+                                
+                                </th>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+                                Educational Delivery Methodologies
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+                                Evidence Of Mastery
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+                                Comments
+                                </td>
+                            </tr>
+                        </thead>
+                        
+                        <tbody className="text-xs text-gray-700 uppercase bg-[#ebeef2] ">
+                            <tr>
+                                <th scope="col" className="px-6 py-3 whitespace-nowrap border-2 border-[#dcebfb]">
+                                Standard :
+                                </th>
+                                <th scope="col" className="px-6 py-3 whitespace-nowrap border-2 border-[#dcebfb]">
+                                
+                                </th>
+                                <th scope="col" className="px-6 py-3 whitespace-nowrap border-2 border-[#dcebfb]">
+                                
+                                </th>
+                                <th scope="col" className="px-6 py-3 whitespace-nowrap border-2 border-[#dcebfb] ">
+                                
+                                </th>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr className="bg-white  hover:bg-gray-50">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap border-2 border-[#dcebfb]">
+                                Cluster 1 :
+                                </th>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+                                    
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+                                    
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+                                    
+                                </td>
+                            </tr>
+                            <tr className="bg-white  hover:bg-gray-50">
+                                
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap border-2 border-[#dcebfb] ">
+                                Cluster 2 :
+                                </th>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+                                    
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap border-2 border-[#dcebfb]">
+                                    
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
-     );
+    );
 }
- 
+
 export default CurriculumPlanning;
--- src/app/post-management/reviews/page.tsx
+++ src/app/post-management/reviews/page.tsx
@@ -3,19 +3,20 @@
 import Link from "next/link";
 
 const Reviews = () => {
-    return ( 
+    return (
         <>
-            <div className="lg:ml-[290px] mt-12 pr-5">
+            <div className="lg:ml-[290px] mt-12 pr-5 overflow-x-auto md:px-6">
                 <div className="flex justify-left gap-5 text-[23px] font-bold mb-5 ml-4">
                     <Link href="/post-management">
                         Post
                     </Link>
                     <Link href="/post-management/reviews" className="text-blue-500 underline">
-                    Reviews
+                        Reviews
                     </Link>
                 </div>
-                <div className="grid w-full h-full justify-center">
-                    <div className="grid grid-cols-3 justify-center mb-5 gap-4">
+                <div className="overflow-x-auto grid w-full justify-center">
+                <div className="grid w-full h-full justify-center overflow-x-auto">
+                    <div className="grid grid-cols-3 justify-center mb-5 gap-4 w-full overflow-x-auto">
                         <div className="grid bg-white p-2 rounded-xl">
                             <p className="font-semibold">Total Reviews</p>
                             <h1 className="font-bold text-[18px]">2K</h1>
@@ -32,105 +33,86 @@ const Reviews = () => {
                             <h1 className="text-gray-400 text-[12px]"> <span className="text-green-500 font-semibold">4.63%</span> vs. last Year</h1>
                         </div>
                     </div>
-                <table className="w-[1000px] h-[600px] overflow-x-auto text-sm text-left rtl:text-right text-gray-500 ">
-
-<tbody>
-    <tr className="bg-white border-b  hover:bg-gray-50">
-        <th scope="row" className=" flex items-center px-6 py-4 h-full text-[25px] font-medium text-gray-900 whitespace-nowrap ">
-            <div className="flex items-center gap-2 text-[15px] text-black ">
-                <img src="/images/me.jpg" className="w-[40px] h-[40px] mr-2 rounded-full" alt="#" />
-                <span className="grid">
-                    <p>Mostapha Taha</p>
-                    <p>ID: <span className="text-gray-400">1385</span></p>
-                </span>
-            </div>
-        </th>
-
-        <td className="px-6 py-4 ">
-            <div className="grid grid-cols-1 gap-3">
-
-                Ask CDCR San Quintin State Prison 2008.<br/> We installed Purex dispensers throughout the prison to comba
-
-
-            </div>
-        </td>
-        <td className="px-6 py-4 ">
-            <div className="grid grid-cols-2 gap-3">
-
-                <button><svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                </svg></button>
-
-
-            </div>
-        </td>
-    </tr>
-    <tr className="bg-white border-b  hover:bg-gray-50">
-        <th scope="row" className=" flex items-center px-6 py-4 h-full text-[25px] font-medium text-gray-900 whitespace-nowrap ">
-            <div className="flex items-center gap-2 text-[15px] text-black ">
-                <img src="/images/me.jpg" className="w-[40px] h-[40px] mr-2 rounded-full" alt="#" />
-                <span className="grid">
-                    <p>Mostapha Taha</p>
-                    <p>ID: <span className="text-gray-400">1385</span></p>
-                </span>
-            </div>
-        </th>
-
-        <td className="px-6 py-4 ">
-            <div className="grid grid-cols-1 gap-3">
-
-                Ask CDCR San Quintin State Prison 2008.<br/> We installed Purex dispensers throughout the prison to comba
-
-
-            </div>
-        </td>
-        <td className="px-6 py-4 ">
-            <div className="grid grid-cols-2 gap-3">
-
-                <button><svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                </svg></button>
-
-
-            </div>
-        </td>
-    </tr>
-    <tr className="bg-white border-b  hover:bg-gray-50">
-        <th scope="row" className=" flex items-center px-6 py-4 h-full text-[25px] font-medium text-gray-900 whitespace-nowrap ">
-            <div className="flex items-center gap-2 text-[15px] text-black ">
-                <img src="/images/me.jpg" className="w-[40px] h-[40px] mr-2 rounded-full" alt="#" />
-                <span className="grid">
-                    <p>Mostapha Taha</p>
-                    <p>ID: <span className="text-gray-400">1385</span></p>
-                </span>
-            </div>
-        </th>
-        <td className="px-6 py-4 ">
-            <div className="grid grid-cols-1  gap-3">
-
-                Ask CDCR San Quintin State Prison 2008.<br/> We installed Purex dispensers throughout the prison to comba
-
-
-            </div>
-        </td>
-        <td className="px-6 py-4 ">
-            <div className="grid grid-cols-2 gap-3">
-
-                <button><svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                </svg></button>
-
-
-            </div>
-        </td>
-    </tr>
-    
-</tbody>
-</table>
+                    </div>
+                <div className="overflow-x-auto flex w-full justify-center">
+                    <table className="w-[1000px] h-[600px] overflow-x-auto text-sm text-left rtl:text-right text-gray-500  ">
+                        <tbody>
+                            <tr className="bg-white border-b  hover:bg-gray-50">
+                                <th scope="row" className=" flex items-center px-6 py-4 h-full text-[25px] font-medium text-gray-900 whitespace-nowrap ">
+                                    <div className="flex items-center gap-2 text-[15px] text-black ">
+                                        <img src="/images/me.jpg" className="w-[40px] h-[40px] mr-2 rounded-full" alt="#" />
+                                        <span className="grid">
+                                            <p>Mostapha Taha</p>
+                                            <p>ID: <span className="text-gray-400">1385</span></p>
+                                        </span>
+                                    </div>
+                                </th>
+                                <td className="px-6 py-4 ">
+                                    <div className="grid grid-cols-1 gap-3">
+                                        Ask CDCR San Quintin State Prison 2008.<br /> We installed Purex dispensers throughout the prison to comba
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <div className="grid grid-cols-2 gap-3">
+                                        <button><svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg></button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr className="bg-white border-b  hover:bg-gray-50">
+                                <th scope="row" className=" flex items-center px-6 py-4 h-full text-[25px] font-medium text-gray-900 whitespace-nowrap ">
+                                    <div className="flex items-center gap-2 text-[15px] text-black ">
+                                        <img src="/images/me.jpg" className="w-[40px] h-[40px] mr-2 rounded-full" alt="#" />
+                                        <span className="grid">
+                                            <p>Mostapha Taha</p>
+                                            <p>ID: <span className="text-gray-400">1385</span></p>
+                                        </span>
+                                    </div>
+                                </th>
+                                <td className="px-6 py-4 ">
+                                    <div className="grid grid-cols-1 gap-3">
+                                        Ask CDCR San Quintin State Prison 2008.<br /> We installed Purex dispensers throughout the prison to comba
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <div className="grid grid-cols-2 gap-3">
+                                        <button><svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg></button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr className="bg-white border-b  hover:bg-gray-50">
+                                <th scope="row" className=" flex items-center px-6 py-4 h-full text-[25px] font-medium text-gray-900 whitespace-nowrap ">
+                                    <div className="flex items-center gap-2 text-[15px] text-black ">
+                                        <img src="/images/me.jpg" className="w-[40px] h-[40px] mr-2 rounded-full" alt="#" />
+                                        <span className="grid">
+                                            <p>Mostapha Taha</p>
+                                            <p>ID: <span className="text-gray-400">1385</span></p>
+                                        </span>
+                                    </div>
+                                </th>
+                                <td className="px-6 py-4 ">
+                                    <div className="grid grid-cols-1  gap-3">
+                                        Ask CDCR San Quintin State Prison 2008.<br /> We installed Purex dispensers throughout the prison to comba
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <div className="grid grid-cols-2 gap-3">
+                                        <button><svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg></button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
                 </div>
             </div>
         </>
-     );
+    );
 }
- 
+
 export default Reviews;
