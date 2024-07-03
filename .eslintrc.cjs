e7a80cf61214ed5530ce9bfe27f08ec53476a539
Mostapha Taha <darsh.taha12382@gmail.com>
2024-07-03 19:05:20 +0300

    eslint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,31 @@
 /** @type {import("eslint").Linter.BaseConfig} */
 
 const config = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
-  root: true,
-  rules: {
-    "@typescript-eslint/prefer-optional-chain": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/prefer-nullish-coalescing": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/require-await": "warn",
-    "@typescript-eslint/no-floating-promises": "warn",
-    "@typescript-eslint/no-misused-promises": "warn",
-    "@typescript-eslint/no-unsafe-assignment": "warn",
-    "@typescript-eslint/no-unsafe-member-access": "warn",
-    "@typescript-eslint/no-unsafe-call": "warn",
-    "@typescript-eslint/no-unsafe-return": "warn",
-    "@typescript-eslint/no-unsafe-argument": "warn",
-  },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: true,
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "next/core-web-vitals",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+    ],
+    root: true,
+    rules: {
+        "@typescript-eslint/prefer-optional-chain": "off",
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/prefer-nullish-coalescing": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+    },
 };
 
 module.exports = config;
