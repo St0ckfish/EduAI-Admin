b4c100d038f62bbe4bf0f46b2a939fbc70aa5608
Mostapha Taha <darsh.taha12382@gmail.com>
2024-07-05 21:15:23 +0300

    Employe page

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -165,7 +165,7 @@ const NavBar = () => {
                                 <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" /><circle cx="9" cy="7" r="4" /><path d="M22 21v-2a4 4 0 0 0-3-3.87" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /></svg>
                                 Profile
                               </Link>
-                              <Link className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:text-white hover:bg-red-500 focus:ring-2 focus:ring-blue-500" href="/">
+                              <Link className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:text-white hover:bg-red-500 focus:ring-2 focus:ring-blue-500" href="/login">
                                 Sign out
                               </Link>
                             </div>
