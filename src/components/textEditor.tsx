4e9a6a3582201b727470b159b762a1a7b505f518
Mostapha Taha <darsh.taha12382@gmail.com>
2024-08-23 21:17:34 +0300

    space

--- src/components/textEditor.tsx
+++ src/components/textEditor.tsx
@@ -57,7 +57,7 @@ const TextEditor = ({ value, onChange, placeholder }: { value: string, onChange:
           type='button'
           onClick={() => formatText('strikeThrough')}
           className="px-3 py-2 bg-gray-200 hover:bg-gray-300 rounded"
-        >
+        > 
           <svg className="h-6 w-6 text-black"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="5" y1="12" x2="19" y2="12" />  <path d="M16 6.5a4 2 0 0 0 -4 -1.5h-1a3.5 3.5 0 0 0 0 7" />  <path d="M16.5 16a3.5 3.5 0 0 1 -3.5 3h-1.5a4 2 0 0 1 -4 -1.5" /></svg>
         </button>
         <button
