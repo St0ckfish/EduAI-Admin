fc2bfd0f06ef8d0ee833212d3f0cd6e3b1294300
M0staphaTaha <darsh.taha12382@gmail.com>
2024-10-16 18:28:28 +0300

    Confirm password

--- src/app/notifies/page.tsx
+++ src/app/notifies/page.tsx
@@ -163,7 +163,10 @@ const Notifies = () => {
                     {notifi.read ? (
                       ""
                     ) : (
-                      <div className="ml-1 flex h-2.5 w-2.5 rounded-full bg-primary"></div>
+                      <span className="relative flex h-3 w-3">
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                      </span>
                     )}{" "}
                   </h1>
                   <div
